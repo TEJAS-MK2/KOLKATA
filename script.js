7741286f31d68d329d815347ffbdad830f083193
Su-57 <omarchyhyprland@gmail.com>
2026-09-14 00:14:27 +0530

    Fix explorer archive photo URLs and map coordinates

--- script.js
+++ script.js
@@ -42,20 +42,18 @@ const pandals = [
   {
     name:'Bagbazar Sarbojanin', zone:'North', area:'Bagbazar', lat:22.60121, lng:88.36682,
     tag:'Heritage · traditional', rating:4.8, ratingSource:'Durga Puja Kolkata directory',
-    photo:'https://upload.wikimedia.org/wikipedia/commons/e/e7/%E0%A6%AC%E0%A6%BE%E0%A6%97%E0%A6%AC%E0%A6%9C%E0%A6%BE%E0%A6%B0_%E0%A6%B8%E0%A6%BE%E0%A6%B0%E0%A7%8D%E0%A6%AC%E0%A6%9C%E0%A6%A8%E0%A7%80%E0%A6%A8_%E0%A6%A6%E0%A7%81%E0%A6%B0%E0%A7%8D%E0%A6%97%E0%A7%8B%E0%A7%8E%E0%A6%B8%E0%A6%AC_%E0%A7%A8%E0%A7%A6%E0%A7%A7%E0%A7%AE.jpg',
-    photoLabel:'Archive photo · 2018', photoSource:'https://commons.wikimedia.org/wiki/File:%E0%A6%AC%E0%A6%BE%E0%A6%97%E0%A6%AC%E0%A6%9C%E0%A6%BE%E0%A6%B0_%E0%A6%B8%E0%A6%BE%E0%A6%B0%E0%A7%8D%E0%A6%AC%E0%A6%9C%E0%A6%A8%E0%A7%80%E0%A6%A8_%E0%A6%A6%E0%A7%81%E0%A6%B0%E0%A7%8D%E0%A6%97%E0%A7%8B%E0%A7%8E%E0%A6%B8%E0%A6%AC_%E0%A7%A8%E0%A7%A6%E0%A7%A7%E0%A7%AE.jpg',
+    photo:'https://upload.wikimedia.org/wikipedia/commons/e/e7/%E0%A6%AC%E0%A6%BE%E0%A6%97%E0%A6%AC%E0%A6%9C%E0%A6%BE%E0%A6%B0_%E0%A6%B8%E0%A6%BE%E0%A6%B0%E0%A7%8D%E0%A6%AC%E0%A6%9C%E0%A6%A8%E0%A7%80%E0%A6%A8_%E0%A6%A6%E0%A7%81%E0%A6%B0%E0%A7%8D%E0%A6%97%E0%A7%8B%E0%A7%8E%E0%A6%B8%E0%A6%AC_%E0%A7%A8%E0%A7%A6%E0%A7%A7%E0%A7%AE.jpg', photoLabel:'Archive photo · 2018', photoSource:'https://commons.wikimedia.org/wiki/File:%E0%A6%AC%E0%A6%BE%E0%A6%97%E0%A6%AC%E0%A6%9C%E0%A6%BE%E0%A6%B0_%E0%A6%B8%E0%A6%BE%E0%A6%B0%E0%A7%8D%E0%A6%AC%E0%A6%9C%E0%A6%A8%E0%A7%80%E0%A6%A8_%E0%A6%A6%E0%A7%81%E0%A6%B0%E0%A7%8D%E0%A6%97%E0%A7%8B%E0%A6%A4%E0%A7%8D%E0%A6%B8%E0%A6%AC_%E0%A7%A8%E0%A7%A6%E0%A7%A7%E0%A7%AE.jpg',
     source:'https://www.durgapujakolkata.in/paras/bagbazar-sarbojanin-durgotsav-exhibition'
   },
   {
-    name:'Kumartuli Park', zone:'North', area:'Kumartuli', lat:22.5967, lng:88.3629,
+    name:'Kumartuli Park', zone:'North', area:'Kumartuli', lat:22.599125, lng:88.361565,
     tag:'Artisan quarter',
-    photo:'https://www.durgapujopandals.com/pandals/?region=North+Kolkata', photoLabel:'Pandal directory photo', photoSource:'https://www.durgapujopandals.com/pandals/?region=North+Kolkata',
+    photo:'https://commons.wikimedia.org/wiki/Special:FilePath/DurgaPuja2019_-_Durga_Puja_pandal_of_Kumartoli_Park_in_Kolkata_01.jpg', photoLabel:'Archive photo · 2019', photoSource:'https://commons.wikimedia.org/wiki/File:DurgaPuja2019_-_Durga_Puja_pandal_of_Kumartoli_Park_in_Kolkata_01.jpg',
     source:'https://www.durgapujopandals.com/pandals/?region=North+Kolkata'
   },
   {
     name:'Shobhabazar Rajbari', zone:'North', area:'Shobhabazar', lat:22.5974, lng:88.3672,
-    tag:'Historic puja',
-    source:'https://www.durgapujakolkata.in/paras'
+    tag:'Historic puja', source:'https://www.durgapujakolkata.in/paras'
   },
   {
     name:'College Square', zone:'Central', area:'College Street', lat:22.57453, lng:88.36447,
@@ -72,7 +70,7 @@ const pandals = [
   {
     name:'Maddox Square', zone:'South', area:'Ballygunge', lat:22.52656, lng:88.35465,
     tag:'Classic adda',
-    photo:'https://www.flickr.com/photos/23985194%40N06/9723469757/', photoLabel:'Archive photo · 2012', photoSource:'https://www.flickr.com/photos/23985194%40N06/9723469757/',
+    photo:'https://commons.wikimedia.org/wiki/Special:FilePath/Pandal_Maddox_Square_2010_Arnab_Dutta.JPG', photoLabel:'Archive photo · 2010', photoSource:'https://commons.wikimedia.org/wiki/File:Pandal_Maddox_Square_2010_Arnab_Dutta.JPG',
     source:'https://www.agamoni.in/pandals/pandal-maddox-square-durga-pujo'
   },
   {
