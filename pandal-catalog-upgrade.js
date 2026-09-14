b6abc21429b0f2d0bc3cbb442023b776285cedf6
Su-57 <omarchyhyprland@gmail.com>
2026-09-14 11:34:52 +0530

    Fix archive image URLs

--- pandal-catalog-upgrade.js
+++ pandal-catalog-upgrade.js
@@ -5,7 +5,6 @@
   ].map(([name,zone,area]) => ({name,zone,area}));
 
   const imageMap = {
-    'Simla Sporting Club':'https://commons.wikimedia.org/wiki/Special:FilePath/Durga_Puja_Pandal_-_Simla_Sporting_Club_-_Kolkata.jpg',
     'Falguni Sangha':'https://commons.wikimedia.org/wiki/Special:FilePath/Durga_Puja_Pandal_-_Falguni_Sangha_-_Suren_Tagore_Road_-_Kolkata_2013-10-11_3350.JPG',
     'Park Circus Beniapukur':'https://commons.wikimedia.org/wiki/Special:FilePath/Durga_Puja_Pandal_Inside_-_Park_Circus_Beniapukur_-_Kolkata_2011-10-04_00715.jpg',
     '74 Pally':'https://commons.wikimedia.org/wiki/Special:FilePath/Durga_Puja_Pandal_-_74_Pally_Part_-_Kolkata_2011-10-03_030270.JPG',
@@ -13,8 +12,8 @@
     'Bosepukur Talbagan':'https://commons.wikimedia.org/wiki/Special:FilePath/Durga_Puja_Pandal_-_Bosepukur_Talbagan_-_Kasba_-_Kolkata_2012-10-23_1178.JPG',
     'Chetla Agrani':'https://commons.wikimedia.org/wiki/Special:FilePath/Durga_Puja_Pandal_-_Chetla_Agrani_Club_-_Kolkata_2017-09-26_4199.JPG',
     'Ekdalia Evergreen':'https://commons.wikimedia.org/wiki/Special:FilePath/Durga_Puja_Pandal_-_Ekdalia_Evergreen_-_Ekdalia_Road_-_Kolkata_2013-10-11_3312.JPG',
-    'Jodhpur Park 95 Pally':'https://commons.wikimedia.org/wiki/Special:FilePath/Durga_Puja_Pandal_-_Jodhpur_Park_-_Kolkata_2017-09-26_4067-4070.tif',
-    'Tridhara Sammilani':'https://commons.wikimedia.org/wiki/Special:FilePath/Durga_Puja_Pandal_-_Tridhara_Sammilani_-_Manohar_Pukur_Road_-_Kolkata_2014-10-02_9048-9050.TIF',
+    'Jodhpur Park 95 Pally':'https://commons.wikimedia.org/wiki/Special:FilePath/Jodhpur_Park.jpg',
+    'Tridhara Sammilani':'https://commons.wikimedia.org/wiki/Special:FilePath/Durga_Puja_Pandal_-_Tridhara_Sammilani_-_Manohar_Pukur_Road_-_Kolkata_2017-09-26_3988.JPG',
     'Singhi Park':'https://commons.wikimedia.org/wiki/Special:FilePath/Durga_Puja_Pandal_Singhi_Park_-_Dover_Lane_-_Kolkata_2013-10-11_3410.JPG',
     'Bosepukur Sitala Mandir':'https://commons.wikimedia.org/wiki/Special:FilePath/Durga_Puja_Pandal_-_Bosepukur_Sitala_Mandir_-_Kasba_-_Kolkata_2012-10-23_1154.JPG',
     'Barisha Sporting Club':'https://commons.wikimedia.org/wiki/Special:FilePath/Durga_Puja_Pandal_-_Barisha_Sporting_Club_-_Kolkata_2012-10-23_1147.JPG'
