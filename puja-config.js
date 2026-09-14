82fa98290836ff0f98db30b9fe4fd214d1456092
Su-57 <omarchyhyprland@gmail.com>
2026-09-14 20:06:05 +0530

    Add shared Puja configuration

---
+++ puja-config.js
@@ -0,0 +1,12 @@
+(() => {
+  'use strict';
+  window.KolkataPujaConfig = Object.freeze({
+    emergency: Object.freeze([
+      Object.freeze({number:'112', label:'India emergency response'}),
+      Object.freeze({number:'101', label:'Fire'}),
+      Object.freeze({number:'108', label:'Ambulance'}),
+      Object.freeze({number:'1091', label:'Women helpline'}),
+      Object.freeze({number:'139', label:'Railway assistance'})
+    ])
+  });
+})();
