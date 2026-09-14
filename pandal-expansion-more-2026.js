90a829f276bce190da35415ad69825821f8ea6cb
Su-57 <omarchyhyprland@gmail.com>
2026-09-14 22:38:08 +0530

    Add verified pins and matched archive photos to more Kolkata pandals

--- pandal-expansion-more-2026.js
+++ pandal-expansion-more-2026.js
@@ -15,9 +15,10 @@
     'Samajsebi Sangha':{zone:'South',area:'Lake Road',lat:22.51474,lng:88.35600,tag:'Verified location',photo:'https://commons.wikimedia.org/wiki/Special:FilePath/Samaj_Sebi_Sangha_Durga_Puja_in_South_Kolkata_(Lake_View_Road)_02.jpg'},
     'Singhi Park':{zone:'South',area:'Gariahat',lat:22.52122,lng:88.36301,tag:'Verified location',photo:'https://commons.wikimedia.org/wiki/Special:FilePath/Durga_Puja_Pandal_-_Singhi_Park_-_Dover_Lane_-_Kolkata_2014-10-02_8943.JPG'},
     'Tarun Matri Sevak Samity':{zone:'South',area:'Behala',lat:22.5022,lng:88.3215,tag:'Verified location',photo:'https://commons.wikimedia.org/wiki/Special:FilePath/Behala_Tarun_Matri_Sevak_Samity_Durga_puja_2025_01.jpg'},
-    'Kidderpore 25 Pally':{zone:'South',area:'Kidderpore',lat:22.53848,lng:88.32660,tag:'Verified location',photo:'https://commons.wikimedia.org/wiki/Special:FilePath/Durga_Puja_Pandal_-_25_Pally_-_Kolkata_2011-10-03_030258.JPG'},
-    'Kidderpore 74 Pally':{zone:'South',area:'Kidderpore',lat:22.53978,lng:88.32512,tag:'Verified location',photo:'https://commons.wikimedia.org/wiki/Special:FilePath/Durga_Puja_Pandal_-_74_Pally_-_Kolkata_2011-10-03_030266.JPG'},
-    'Kidderpore 75 Pally':{zone:'South',area:'Kidderpore',lat:22.54030,lng:88.32528,tag:'Verified location',photo:''}
+    'Bakul Bagan':{zone:'South',area:'Bhowanipore',lat:22.52676,lng:88.34826,tag:'Verified location',photo:'https://commons.wikimedia.org/wiki/Special:FilePath/Bakul_Bagan_Sarbojanin_Arnab_Dutta_2011.jpg'},
+    'Dumdum Park Tarun Dal':{zone:'North',area:'Dum Dum Park',lat:22.61173,lng:88.41874,tag:'Verified location',photo:'https://commons.wikimedia.org/wiki/Special:FilePath/Dum_Dum_Park_Tarun_Dal_2023.jpg'},
+    'Chorebagan Sarbojanin':{zone:'North',area:'Chorebagan',lat:22.583485,lng:88.363380,tag:'Verified location',photo:'https://commons.wikimedia.org/wiki/Special:FilePath/Durga_Puja_2024_at_Chorbagan_Sarbojanin_05.jpg'},
+    'Muhammad Ali Park':{zone:'Central',area:'Central Kolkata',lat:22.57710,lng:88.36030,tag:'Verified location',photo:'https://commons.wikimedia.org/wiki/Special:FilePath/5465g_muhammad-ali-park_pratima-foeticide.jpg'}
   };
   window.KOLKATA_MORE_PINS=Object.freeze(Object.fromEntries(Object.entries(MORE).map(([name,p])=>[name,[p.lat,p.lng]])));
   window.KOLKATA_PIN_METADATA=window.KOLKATA_PIN_METADATA||{};
@@ -30,14 +31,11 @@
     ['Chaltabagan Sarbojanin','North','Chaltabagan','https://commons.wikimedia.org/wiki/Special:FilePath/Durga_Puja_2025_at_Chalta_Bagan_Sarbojanin_18.jpg'],
     ['Dum Dum Park Tarun Sangha','North','Dum Dum Park','https://commons.wikimedia.org/wiki/Special:FilePath/Dum_Dum_Park_Tarun_Sangha_2023.jpg'],
     ['Behala Club','South','Behala','https://commons.wikimedia.org/wiki/Special:FilePath/Behala_Club.jpg'],
-    ['Bakul Bagan Sarbojanin','South','Bakul Bagan',''],
+    ['Bakul Bagan Sarbojanin','South','Bakul Bagan','https://commons.wikimedia.org/wiki/Special:FilePath/Bakul_Bagan_Sarbojanin_Arnab_Dutta_2011.jpg'],
     ['Pratapaditya Road Tricon Park','South','Kalighat',''],
     ['Vivekananda Sporting Club','South','Haridevpur',''],
     ['41 Pally Club','South','Haridevpur','https://commons.wikimedia.org/wiki/Special:FilePath/41_Pally_Durga_puja_2025_11.jpg'],
-    ['Roy Bari, Behala','South','Behala','https://commons.wikimedia.org/wiki/Special:FilePath/The_2025_Durga_Puja_at_Amarendra_Bhawan_%28Behala_Roy_Bari%29_05.jpg'],
-    ['Bosepukur Talbagan','South','Bosepukur, Kasba','https://commons.wikimedia.org/wiki/Special:FilePath/Durga_Puja_Pandal_-_Bosepukur_Talbagan_-_Kasba_-_Kolkata_2012-10-23_1190.JPG'],
-    ['Chorebagan Sarbojanin','North','Simla/Girish Park',''],
-    ['22 Palli Sarodotsab','South','Bakul Bagan, Bhowanipore','']
+    ['Roy Bari, Behala','South','Behala','https://commons.wikimedia.org/wiki/Special:FilePath/The_2025_Durga_Puja_at_Amarendra_Bhawan_%28Behala_Roy_Bari%29_05.jpg']
   ].map(([name,zone,area,photo])=>({name,zone,area,photo}));
   window.KOLKATA_EXTRA_PANDALS=Object.freeze(EXTRA);
   const escapeHtml=s=>String(s).replace(/[&<>\"']/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;','\"':'&quot;',"'":'&#39;'}[c]));
